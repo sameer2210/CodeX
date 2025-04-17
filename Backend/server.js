ddac2822b10b676b0b570ab9cb0e32a88517fe4f
sameer2210 <sameerkhan27560@gmail.com>
2025-04-17 17:43:37 +0530

    Sockit_io

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,30 @@
 import app from "./src/app.js";
 import connectToDb from "./src/db/db.js";
+import {Server as SocketServer} from 'socket.io';
+import http, { METHODS } from 'http';
+import { Socket } from "dgram";
 
 connectToDb();
 
+const server = http.createServer(app);
+const io = new SocketServer(server,{
+  cors:{
+    origin:'*',
+    // methods:["GET","POST"]
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('New client Connected');
+  socket.on('disconnect',() => {
+    console.log('Client is Disconnect');
+    
+  })
+  
+})
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, (req, res) => {
+server.listen(port, (req, res) => {
   console.log(`server is running on port ${port}`);
 });
--- Frontend/src/views/home/project/Project.jsx
+++ Frontend/src/views/home/project/Project.jsx
@@ -1,36 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Project.css";
+import { io } from "socket.io-client";
 
 const Project = () => {
   const prams = useParams();
-  const [messages,setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    "klsdj","lksdjf"
+  ]);
+
+  useEffect(() => {
+    io("http://localhost:3000");
+  });
 
   return (
     <main className="project-main">
       <section className="project-section">
         <div className="chat">
-
           <div className="messages">
-            {
-                messages.map((message) => {
-                  return (
-                    <div className="message">
-                      <span>{message}</span>
-                    </div>
-                  )
-                })
-            }
+            {messages.map((message) => {
+              return (
+                <div className="message">
+                  <span>{message}</span>
+                </div>
+              );
+            })}
           </div>
 
           <div className="input-area">
-
-            <input type="text" placeholder="message to project...."/>
-            <button><i className="ri-send-plane-2-fill"></i></button>
-
+            <input type="text" placeholder="message to project...." />
+            <button>
+              <i className="ri-send-plane-2-fill"></i>
+            </button>
           </div>
-
         </div>
         <div className="code"></div>
         <div className="review"></div>
